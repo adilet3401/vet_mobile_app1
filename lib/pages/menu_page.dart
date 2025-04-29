f4da3cb9531742502170e4e3b2b7d6efa694054b
adilet <adilet.dildeev08@gmail.com>
2025-04-29 16:08:41 +0600

    animate fixed

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -36,35 +36,32 @@ class _MenuPageState extends State<MenuPage> {
     return Scaffold(
       bottomNavigationBar: ClipRRect(
         borderRadius: BorderRadius.circular(40), // Скругление углов
-        child: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          type: BottomNavigationBarType.fixed,
-          // selectedItemColor: Colors.white, // Цвет для выбранного элемента
-          // ignore: deprecated_member_use
-          // unselectedItemColor: Colors.white.withOpacity(
-          //   0.6,
-          // ), // Цвет для невыбранных элементов
-          // selectedLabelStyle: TextStyle(
-          //   color: Colors.white,
-          //   fontWeight: FontWeight.w500,
-          // ),
-          currentIndex: myCurrentIndex,
-          onTap: (index) {
-            setState(() {
-              myCurrentIndex = index;
-            });
-            _pageController.animateToPage(
-              index,
-              duration: Duration(milliseconds: 230),
-              curve: Curves.easeInOut,
-            );
-          },
-          items: [
-            BottomNavBar('assets/Vector-7.svg', 0),
-            BottomNavBar('assets/Vector-8.svg', 1),
-            BottomNavBar('assets/Group-3.svg', 2),
-          ],
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            splashFactory: NoSplash.splashFactory, // Отключение эффекта нажатия
+            highlightColor: Colors.transparent, // Убираем подсветку
+          ),
+          child: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            type: BottomNavigationBarType.fixed,
+            currentIndex: myCurrentIndex,
+            onTap: (index) {
+              setState(() {
+                myCurrentIndex = index;
+              });
+              _pageController.animateToPage(
+                index,
+                duration: Duration(milliseconds: 230),
+                curve: Curves.easeInOut,
+              );
+            },
+            items: [
+              BottomNavBar('assets/Vector-7.svg', 0),
+              BottomNavBar('assets/Vector-8.svg', 1),
+              BottomNavBar('assets/Group-3.svg', 2),
+            ],
+          ),
         ),
       ),
       body: PageView(
@@ -79,15 +76,17 @@ class _MenuPageState extends State<MenuPage> {
 
   BottomNavigationBarItem BottomNavBar(String iconPath, int currentIndex) {
     return BottomNavigationBarItem(
-      icon: CircleAvatar(
-        backgroundColor:
-            myCurrentIndex == currentIndex ? Colors.white : Color(0xff01a560),
-        minRadius: 26,
+      icon: Container(
+        decoration: BoxDecoration(
+          color:
+              myCurrentIndex == currentIndex ? Colors.white : Color(0xff01a560),
+          shape: BoxShape.circle,
+        ),
+        padding: const EdgeInsets.all(10), // Отступы для иконки
         child: SvgPicture.asset(
           iconPath,
           width: 24,
           height: 24,
-          // ignore: deprecated_member_use
           color:
               myCurrentIndex == currentIndex ? Color(0xff01A560) : Colors.white,
         ),
